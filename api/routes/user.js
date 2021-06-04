3f19c33b5915943d5ccbdc4d5096a9bd95fc3b2e
Geetha Priyakarikebalakrishna(UST <U63071@ust-global.com>
2021-06-04 15:44:41 +0530

    jwt token

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+
 
 const User = require('../models/user');
 const saltRounds = 10;
@@ -51,13 +53,59 @@ router.post('/signup', (req, res, next) => {
         })
 });
 
+router.post('/login', (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: 'Auth failed/Unauthorized'
+                });
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: 'Auth failed/Unauthorized'
+                    });
+                }
+                if (result) {
+                    const token = jwt.sign(
+                            {
+                                email: user[0].email,
+                                userId: user[0]._id
+                            },
+                            process.env.JWT_KEY,
+                            {
+                                expiresIn: "1h"
+                            }
+                        );
+                  
+                    return res.status(200).json({
+                        message: 'Auth successful',
+                        token:token
+                    });
+                }
+                return res.status(200).json({
+                    message: 'Auth failed/Unauthorized'
+                });
+            }
+            );
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
 router.delete('/:userId', (req, res, next) => {
     User.remove({ _id: req.params.userId })
         .exec()
         .then(result => {
             console.log(result);
             res.status(200).json({
-                email:result.email,
+                email: result.email,
                 message: 'user deleted'
             })
         })
@@ -69,8 +117,5 @@ router.delete('/:userId', (req, res, next) => {
         }
         );
 })
-
-
-
 
 module.exports = router;
